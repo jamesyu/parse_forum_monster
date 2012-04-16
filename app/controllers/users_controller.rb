6b4a1ce05b10d6b403355b28351a117f9ba7c82e
James Yu <jamesjacobyu@gmail.com>
2012-04-16 13:03:11 -0700

    don't pass in password confirmation to parse

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,10 @@ class UsersController < ApplicationController
     @user = User.new  
   end  
     
-  def create  
-    @user = User.new(params[:user])  
+  def create
+    parse_params = params[:user].clone
+    parse_params.delete :password_confirmation
+    @user = User.new(parse_params)
     if @user.save
       LocalUser.create! :username => @user.username, :created_at => @user.created_at
       redirect_to root_url, :notice => "Signed up!"  
