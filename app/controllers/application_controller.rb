7f63da4b49709590f2e34fbd5cc3cb978e48dee0
James Yu <jamesjacobyu@gmail.com>
2012-04-17 16:50:49 -0700

    add current_parse_user helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,20 @@
 class ApplicationController < ActionController::Base  
   protect_from_forgery  
-  helper_method :current_user  
+  helper_method :current_user, :current_parse_user
     
-  private  
+  private
+
+  def current_parse_user
+    if session[:user_id]
+      @current_parse_user ||= User.find(session[:user_id])
+    else
+      @current_parse_user = nil
+    end
+  end
+  
   def current_user
-    parse_user = User.find(session[:user_id]) if session[:user_id]
-    @current_user ||= if parse_user
-                        LocalUser.find_by_username(parse_user.username)
+    @current_user ||= if current_parse_user
+                        LocalUser.find_by_objectId(current_parse_user.objectId)
                       else
                         nil
                       end
